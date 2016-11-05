b1a8c28e740f03f328bec7dfced90a9da08e0780
weizhou <shiwoow@gmail.com>
2016-11-06 00:19:57 +0800

    Initial Commit

---
+++ LeetCodeInSwift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  LeetCodeInSwift
+//
+//  Created by 纬洲 冯 on 06/11/2016.
+//  Copyright © 2016 dopcn. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
